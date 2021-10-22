7dbc180fe3a77f995834147f53a72e2e4ad372ef
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-10-22 11:54:15 +0700

    register update + database integration

--- app/src/main/java/com/bilocker/activity/RegisterPage.java
+++ app/src/main/java/com/bilocker/activity/RegisterPage.java
@@ -8,14 +8,26 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bilocker.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegisterPage extends AppCompatActivity {
 
     private TextView toLoginTxt;
     private Button registerBtn;
     private EditText usernameTxt,emailTxt,passwordTxt;
+    private Intent toLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +35,12 @@ public class RegisterPage extends AppCompatActivity {
         setContentView(R.layout.activity_register_page);
 
         toLoginTxt = findViewById(R.id.register_to_login_txt);
+        registerBtn = findViewById(R.id.register_btn);
+        usernameTxt = findViewById(R.id.register_username);
+        emailTxt = findViewById(R.id.register_email);
+        passwordTxt = findViewById(R.id.register_password);
+
+        toLogin = new Intent(RegisterPage.this,LoginPage.class);
     }
 
     @Override
@@ -32,8 +50,55 @@ public class RegisterPage extends AppCompatActivity {
         toLoginTxt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent toLogin = new Intent(RegisterPage.this,LoginPage.class);
+
                 startActivity(toLogin);
+            }
+        });
+
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String url = "https://bilocker.000webhostapp.com/BiLocker/Register.php";
+
+                final String name = usernameTxt.getText().toString();
+                final String email = emailTxt.getText().toString();
+                final String password = passwordTxt.getText().toString();
+
+                StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Toast.makeText(getApplication(),response.toString(),Toast.LENGTH_SHORT).show();
+
+                        if (response.equals("Success")){
+                            Toast.makeText(getApplication(),"Register Success",Toast.LENGTH_SHORT).show();
+                            startActivity(toLogin);
+
+                        }else
+                            if(response.equals("Failed")){
+                                Toast.makeText(getApplication(),"Email already Taken",Toast.LENGTH_SHORT).show();
+                                usernameTxt.setText("");
+                                emailTxt.setText("");
+                                passwordTxt.setText("");
+                            }
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getApplication(),"Volley Error",Toast.LENGTH_SHORT).show();
+                    }
+                }){
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> params = new HashMap<String, String>();
+                        params.put("email",email);
+                        params.put("name",name);
+                        params.put("password",password);
+                        return params;
+                    }
+                };
+
+                RequestQueue requestQueue = Volley.newRequestQueue(RegisterPage.this);
+                requestQueue.add(request);
             }
         });
     }
