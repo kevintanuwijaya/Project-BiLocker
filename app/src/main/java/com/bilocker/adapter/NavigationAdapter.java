ca76a41c919a13e71857338bd91f16960eb683e2
KEVIN TANUWIJAYA <kevintanuwijaya11@gmail.com>
2021-10-26 10:18:12 +0700

    drawable updates

---
+++ app/src/main/java/com/bilocker/adapter/NavigationAdapter.java
@@ -0,0 +1,36 @@
+package com.bilocker.adapter;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class NavigationAdapter extends RecyclerView.Adapter<NavigationAdapter.MyViewHolder> {
+
+    private Context context;
+
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
